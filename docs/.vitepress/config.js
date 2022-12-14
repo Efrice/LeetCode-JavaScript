aa5ccba729dae2fbca90c9843c5113b0d3b2a7dc
efrice <719431749@qq.com>
2022-12-14 21:25:11 +0800

    feat: base url

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,6 +2,7 @@ export default {
   title: "efrice",
   description: "前端 LeetCode 刷题算法进阶之路",
   themeConfig: {
+    base: "/",
     sidebar: [
       {
         items: [{ text: "1.两数之和", link: "/problems/1.两数之和" }],
