5f51fb8714c4103f7affc5363c624b7a34caace1
efrice <719431749@qq.com>
2022-12-14 22:15:42 +0800

    feat: [wip] deploy

--- deploy.sh
+++ deploy.sh
@@ -3,25 +3,14 @@
 # abort on errors
 set -e
 
-# build
 pnpm build
 
-# navigate into the build output directory
 cd docs/.vitepress/dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-# just a example, please change the follow line with your github account!!!
- git push -f https://github.com/Efrice/Efrice.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+ git push -f git@github.com:Efrice/Efrice.github.io.git master:gh-pages
 
 cd -
