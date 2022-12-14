6c91517fdfd20e684b4989e5ac0e3b685336a620
efrice <719431749@qq.com>
2022-12-14 21:21:54 +0800

    feat: init project

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,11 @@
+export default {
+  title: "efrice",
+  description: "前端 LeetCode 刷题算法进阶之路",
+  themeConfig: {
+    sidebar: [
+      {
+        items: [{ text: "1.两数之和", link: "/problems/1.两数之和" }],
+      },
+    ],
+  },
+}
